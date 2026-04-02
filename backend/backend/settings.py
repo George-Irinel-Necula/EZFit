4f4e86d3ca526e6dcf29e5eeb4063c45f010b06c
4Mista <irinelnecula@yahoo.com>
2026-04-02 14:47:06 +0300

    Added .env for Security with AzureDB

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,9 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d#@7%f4z@t^dvih62ucw$_-l!$@cj$sa55m2m@7#ftt#rr7a6p'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
